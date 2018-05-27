4db86054c074524a1b0c86d2a9aee1be9b069972
Leticia Molina <letimolina@gmail.com>
2018-05-27 18:42:53 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,16 @@
 def my_hash
-my_hash = {"goats" => 3, "cats" => 2}
+my_hash = {
+"goats" => 3, "cats" => 2
+  
+}
 end
 
 
 def shipping_manifest
-  shipping_manifest = { "whale bone corsets" => 5
-"porcelain vases" => 2
+shipping_manifest = { 
+"whale bone corsets" => 5,
+"porcelain vases" => 2,
 "oil paintings" => 3
-"muskets" => 2
-"gun powder" => 4
 }
 end
 
@@ -19,7 +21,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+ shipping_manifest["oil paintings"]
 
 end
 
@@ -30,13 +32,15 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+ shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
+  shipping_manifest
 
 end
